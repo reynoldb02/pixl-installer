95cd574637f8f5694c50cfefb0e93e2678955621
reynoldb02 <32553180+reynoldb02@users.noreply.github.com>
2019-05-19 18:06:01 -0500

    die

---
+++ sys/apis/class.lua
@@ -0,0 +1,35 @@
+return function(base)
+    local c = { }
+    if type(base) == "table" then
+        for i, v in pairs(base) do
+            c[i] = v
+        end
+        c._base = base
+    end
+    c.__index = c
+    setmetatable(c, {
+        __call = function(class_tbl, ...)
+            local obj = { }
+            setmetatable(obj, c)
+            if class_tbl.init then
+                class.tbl.init(obj, ...)
+            else
+                if base and base.init then
+                    base.init(obj, ...)
+                end
+            end
+            return obj
+        end
+    })
+    
+    c.is_a = 
+        function(self, klass)
+            local m = getmetatable(self)
+            while m do
+                if m == klass then return true end
+                m = m._base
+            end
+            return false
+        end
+    return c
+end
